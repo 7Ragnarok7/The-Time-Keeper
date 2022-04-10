2da8359f0edf89e7490d3bc6f60503ecc270c725
Anurag Mondal <anorak318@gmail.com>
2022-04-10 19:17:29 +0530

    Create termux-install.sh

---
+++ termux-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "\n.....................INSTALLING THE-TIME-KEEPER..........................\n"
+chmod +x ttk
+apt-get update
+apt-get install jq
+mkdir -p ~/../usr/share/ttk/
+cp -f ttk ~/../usr/share/ttk/
+ln -sf ~/../usr/share/ttk/ttk ~/../usr/bin/ttk
+echo -e "\n......................INSTALLATION FINISHED......................"
+echo -e "\nThanks for installing THE-TIME-KEEPER.\nUse ttk -h for usage instructions.\n"
