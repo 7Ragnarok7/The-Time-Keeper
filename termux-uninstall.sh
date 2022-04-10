61c6d401247ab78f8a383c111ebedef7fe49096f
Anurag Mondal <anorak318@gmail.com>
2022-04-10 19:37:21 +0530

    Create termux-uninstall.sh

---
+++ termux-uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm ~/../usr/bin/ttk
+rm -rf ~/../usr/share/ttk/
+echo -e "\n.......................THE-TIME-KEEPER UNINSTALLED............................"
+echo -e "\nIt's sad to see you leave :(\n"
